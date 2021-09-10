04bb3b30dbfa14e5f7486c44f052d36d4fc9aa79
Els Bergen <elsbergen@scarlet.be>
2021-09-10 19:00:01 +0200

    Test van Els Signed-off-by: Els Bergen <elsbergen@scarlet.be>

---
+++ ExtraOef4/solution/solution.nl.py
@@ -0,0 +1,2 @@
+x = int(input("Hoeveel glazen water heb je vandaag al gedronken van 250 cl ?")
+print("Proficiat, je hebt ",x,"glazen water gedronken. En dat kost je" , x * 0.0043, "euro")
--- expressie1/solution/solution.nl.py
+++
@@ -1,3 +0,0 @@
-print (“Hello world”) 
-print (“Hello world”, “ dit is leuk ”) 
-print(“ik heb “ ,2 , “ appels”)	
