25437fc2b72cf158de773585a0e24e0677005fa6
ElsBritta <elsbergen@scarlet.be>
2021-09-22 10:47:08 +0200

    Signed-off-by: ElsBritta <elsbergen@scarlet.be>

---
+++ Oef1BB/solution/solution.nl.py
@@ -0,0 +1,5 @@
+getal1 = float(input("Geef het eerste getal:"))
+getal2 = float(input("Geef het tweede getal:"))
+getal3 = float(input("Geef het derde getal:"))
+print("Het gemiddelde van", getal1, "en", getal2, "en", getal3, "is", (getal1+ getal2+ getal3)/3)
+
