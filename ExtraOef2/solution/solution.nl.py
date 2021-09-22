a793baccf9fe277c4bc0566d1730605b3964ecde
ElsBritta <elsbergen@scarlet.be>
2021-09-22 09:48:46 +0200

    Signed-off-by: ElsBritta <elsbergen@scarlet.be>

---
+++ ExtraOef2/solution/solution.nl.py
@@ -0,0 +1,2 @@
+print (“Je drinkt best 7 glazen water per dag.”) 
+print (7* “mm, water is lekker ”) 
---
+++ ExtraOef4/solution/solution.nl.py
@@ -0,0 +1,2 @@
+x = int(input("Hoeveel glazen water heb je vandaag al gedronken van 250 ml ?")
+print("Proficiat, je hebt ",x,"glazen water gedronken. En dat kost je" ,str( x * 250*0.0043/1000) + "euro")
