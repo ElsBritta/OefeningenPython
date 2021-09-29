4266ab880b57f93f2cd89f7823a4e36b9be7ec17
ElsBritta <elsbergen@scarlet.be>
2021-09-29 15:18:02 +0200

    Signed-off-by: ElsBritta <elsbergen@scarlet.be>

--- HogerLager/solution/solution.nl.py
+++ HogerLager/solution/solution.nl.py
@@ -1,2 +1,19 @@
+import random
+getal1 = random.randint(1,13)
+print(getal1)
+aantal_juist = 0
 
-
+for i in range(7):
+    raden =input("Is het volgende getal hoger (h) of lager (l) ?")
+    getal2= random.randint(1,13)
+    if getal2 > getal1 and raden ==  "h":
+        print("juist geraden")
+        aantal_juist +=1
+    elif getal2 < getal1 and raden == "l":
+        print("juist geraden")
+        aantal_juist +=1
+    else:
+        print("fout geraden")
+    print(getal2)
+    getal1 = getal2
+print("Je hebt", aantal_juist, "keer goed geraden en", 7- aantal_juist,"keer fout geraden.")
--- RaadGetal/solution/solution.nl.py
+++ RaadGetal/solution/solution.nl.py
@@ -1,2 +1,22 @@
+import random
+getal1 = random.randint(1,20)
 
+gevonden = False
+aantal  = 0
+
+while aantal < 6 and not gevonden:
+  x =int(input("Geef een getal"))
+    
+  if x==getal1:
+    gevonden = True
+  elif x < getal1 :
+    print("Het te zoeken getal is groter")
+  else:
+    print("Het te zoeken getal is kleiner")
+  aantal +=1
+    
+if gevonden:
+  print("proficiat, je bent gewonnen")
+else:
+  print("Je hebt verloren.")
 
