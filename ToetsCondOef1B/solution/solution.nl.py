9d81d4764918c67b2fc798a82f1648f8e57b7c58
ElsBritta <elsbergen@scarlet.be>
2021-10-05 21:22:12 +0200

    Signed-off-by: ElsBritta <elsbergen@scarlet.be>

---
+++ ToetsCondOef1B/solution/solution.nl.py
@@ -0,0 +1,11 @@
+getal1 = float(input("Geef eerste getal:"))
+getal2 = float(input("Geef tweede getal:"))
+getal3 = float(input("Geef derde getal:"))
+
+if getal1 > getal2 and getal1 > getal3:
+  print(getal1)
+elif getal2 > getal1 and getal2 > getal3:
+  print(getal2)
+else:
+  print(getal3)  
+
