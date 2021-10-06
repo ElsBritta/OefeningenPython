0d064a80ae2dd0392d3b566b9cf0eabb0fb2188a
ElsBritta <elsbergen@scarlet.be>
2021-10-06 10:26:45 +0200

    Signed-off-by: ElsBritta <elsbergen@scarlet.be>

--- ToetsCondOef2A/solution/solution.nl.py
+++ ToetsCondOef2A/solution/solution.nl.py
@@ -1,2 +1,10 @@
-x = int(input("Geef een getal met 5 cijfers.")
-print(x%1000)
+nummer_tombola = int(input())
+e =nummer_tombola%10
+h = (nummer_tombola//100)%10
+d = nummer_tombola//1000
+if h%2 ==0:
+  print("Proficiat, je hebt een prijs gewonnen!")
+elif 4<= d <=6:
+  print("Je krijgt een troostprijs.")  
+else:
+  print("Jammer, volgende keer meer geluk.")  
