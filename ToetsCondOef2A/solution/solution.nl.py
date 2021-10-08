17f35312c28e1641e09f063d97ec73a1e6c02011
ElsBritta <elsbergen@scarlet.be>
2021-10-08 18:04:54 +0200

    Signed-off-by: ElsBritta <elsbergen@scarlet.be>

--- ToetsCondOef2A/solution/solution.nl.py
+++ ToetsCondOef2A/solution/solution.nl.py
@@ -1,8 +1,8 @@
 nummer_tombola = int(input())
 e =nummer_tombola%10
-h = (nummer_tombola//100)%10
+t = (nummer_tombola//10)%10
 d = nummer_tombola//1000
-if h%2 ==0:
+if t%2 ==0:
   print("Proficiat, je hebt een prijs gewonnen!")
 elif 4<= d <=6:
   print("Je krijgt nog een troostprijs.")  
