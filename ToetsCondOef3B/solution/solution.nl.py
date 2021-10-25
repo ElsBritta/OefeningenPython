2c1013853f645e030acf1b0dfac57de0bf4adea9
PetraWyndaele <92738335+PetraWyndaele@users.noreply.github.com>
2021-10-25 15:44:39 +0200

    Update solution.nl.py

--- ToetsCondOef3B/solution/solution.nl.py
+++ ToetsCondOef3B/solution/solution.nl.py
@@ -3,9 +3,9 @@ e =nummer_tombola%10
 t = (nummer_tombola//10)%10
 h = (nummer_tombola//100)%10
 d = nummer_tombola//1000
-if t%4 ==0:
+if h%3 ==0:
   print("Proficiat, je hebt een prijs gewonnen!")
-elif 2>= e or e>= 8:
+elif 3>= e or e>= 8:
   print("Je krijgt nog een troostprijs.")  
 else:
   print("Jammer, volgende keer meer geluk.")
