d8836804b31bdf2c87f52823b6e77410d700f500
ElsBritta <75781873+ElsBritta@users.noreply.github.com>
2021-12-29 16:52:56 +0100

    Add files via upload

---
+++ solution.nl.py
@@ -0,0 +1,4 @@
+getal1 = float(input("Geef een getal van minstens 2 cijfers:"))
+getal2 = int(input("Geef een geheel getal van 1 tot 9:"))
+print("De rest na deling van", getal1, "door ", getal2, " is ", getal1%getal2)
+
